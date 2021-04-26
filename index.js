116bd55ff75782f37b3bbd04bdd201b8d3bc3902
Abhishek Kumar <>
2021-04-26 13:17:26 +0000

    numbering solution

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const array1=['a','b','c','x']
-const array2=['z','y','x']
+const array1 = ['a', 'b', 'c', 'x']
+const array2 = ['z', 'y', 'x']
 
-function commonItem(arr1,arr2){
+function commonItem(arr1, arr2) {
 
-  return arr1.some(item => arr2.includes(item))
+  // return arr1.some(item => arr2.includes(item)) 1.
 
-// let map = {}
-// for (let i=0; i<arr1.length; i++){
-//   if(!map[arr1[i]]){
-//     const item = arr1[i];
-//     map[item]= true;
-//   }
-// }
-//   for(let j=0; j<arr2.length; j++){
-//     if(map[arr2[j]]){
-//       return true;
-//     }
-//   }
-//   return false;
-
-// O(a+b)
+  // let map = {}               2.
+  // for (let i=0; i<arr1.length; i++){
+  //   if(!map[arr1[i]]){
+  //     const item = arr1[i];
+  //     map[item]= true;
+  //   }
+  // }
+  //   for(let j=0; j<arr2.length; j++){
+  //     if(map[arr2[j]]){
+  //       return true;
+  //     }
+  //   }
+  //   return false;
+  // O(a+b)
 }
 commonItem(array1, array2);
-// function commonItem(arr1,arr2){ 
+// function commonItem(arr1,arr2){      3.
 //   for(let i=0; i<arr1.length; i++){
 //     for(let j=0; j<arr2.length; j++){
 //       if(arr1[i]===arr2[j]){
