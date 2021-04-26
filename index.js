47edae2b3d1a4d6d4b6d60251ab6f4cbb704c5d8
Abhishek Kumar <>
2021-04-26 16:25:04 +0000

    reference type, context, instantiation

--- index.js
+++ index.js
@@ -66,3 +66,41 @@ strings.unshift('x'); //0(n)
 strings.splice(2,0,'aleins') //0(n)
 console.log(strings)
 
+// reference type
+var object1={ value:10 }
+var object2= object1 
+var object3={ value:10 }
+
+// context vs scope
+const object4 = {
+	a: function(){
+		console.log(this)
+	}
+}
+
+console.log(object4.a)
+
+//instatiation
+class Player{
+	constructor(name,type){
+		console.log('player',this)
+		this.name =name;
+		this.type =type;
+	}
+	introducer(){
+		console.log(`Hi I am ${this.name}, I'm a ${this.type}`)
+	}
+}
+
+class Wizard extends Player{
+	constructor(name,type){
+		super(name,type)
+		console.log('wizard',this)
+	}
+	play(){
+		console.log(`Wheeeeeee I'm a ${this.type}`)
+	}
+}
+
+const wizard1 = new Wizard('Shelly', 'Healer')
+// const wizard2 = new Wizard('Shawn', 'Dark Magic')
